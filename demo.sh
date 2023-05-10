415bb5702215fdbb234c32c5096930a7e038c27a
QuyAnh2005 <dangquyanh150101@gmail.com>
2023-05-10 14:00:44 +0700

    remove demo file

--- demo.sh
+++
@@ -1,7 +0,0 @@
-cd monotonic_align
-python setup.py build_ext --inplace
-cd ..
-
-curl -L https://www.dropbox.com/s/e0h13tufx2oobn2/G_523000.pth?dl=0 --output logs/jp_base/model.pth
-
-streamlit run app.py
